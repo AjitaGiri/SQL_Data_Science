9e3bceb05f44c80163fbd87dd417fa2e06cc2e6c
AjitaGiri <ajitagiri07@gmail.com>
2026-05-31 14:20:11 +0545

    Nested Common table expressions

--- Advanced SQL/Common_Table_Expressions.sql
+++ Advanced SQL/Common_Table_Expressions.sql
@@ -46,3 +46,45 @@ left join CTE_total_sale cts
 on c.customer_id= cts.customer_id
 left join CTE_last_order  clo
 on c.customer_id=clo.customer_id;
+
+
+-- Nested CTEs
+ with CTE_total_sale as(
+    select
+    customer_id,
+    sum(sales) as total_sales
+    from Sales.Orders
+    group by customer_id 
+),
+--  Rank customers based on total sales per customer
+CTE_Customer_Rank as(
+    select 
+    customer_id,
+    total_sales,
+    rank() over (order by total_sales DESC) as customer_rank
+    from CTE_total_sale
+),
+-- segment customers based on their total sales (Nested CTE)
+CTE_Customer_Segments as(
+    select
+    customer_id,
+    case
+    when total_sales>100 then 'High'
+    when total_sales >50 then 'Medium'
+    else 'Low'
+    end Customersegments
+    from CTE_total_sale
+)
+select 
+c.customer_id,
+c.first_name,
+c.last_name,
+clo.customer_rank,
+cse.Customersegments
+from Sales.Customers c
+left join CTE_total_sale cts
+on c.customer_id= cts.customer_id
+left join CTE_Customer_Rank  clo
+on c.customer_id=clo.customer_id;
+left join CTE_Customer_Segment cse
+on c.customer_id = cse.customer_id
