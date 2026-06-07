eff7ba176f6902dc5e79803747a593c7734703dd
AjitaGiri <ajitagiri07@gmail.com>
2026-06-07 14:37:36 +0545

    Join in SQL

--- Datacamp/data-driven decision making in SQL/Exercise2.sql
+++ Datacamp/data-driven decision making in SQL/Exercise2.sql
@@ -40,3 +40,45 @@ GROUP BY customer_id
 having count(movie_id)>7 -- Select only customers with more than 7 movie rentals
 ORDER BY avg(rating); -- Order by the average rating in ascending order
 
+-- Join renting and customers
+--For many analyses it is necessary to add customer information to the data in the table renting.
+
+-- Augment the table renting with all columns from the table customers with a LEFT JOIN.
+--Use as alias' for the tables r and c respectively.
+SELECT * -- Join renting with customers
+FROM renting r
+LEFT JOIN customers c
+ON r.customer_id=c.customer_id;
+
+SELECT *
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+where c.country='Belgium'; -- Select only records from customers coming from Belgium
+
+SELECT avg(r.rating) -- Average ratings of customers from Belgium
+FROM renting AS r
+LEFT JOIN customers AS c
+ON r.customer_id = c.customer_id
+WHERE c.country='Belgium';
+
+-- Aggregating revenue, rentals and active customers
+-- The management of MovieNow wants to report key performance indicators (KPIs) for the performance of the company in 2018. They are interested in measuring the financial successes as well as user engagement. Important KPIs are, therefore, the revenue coming from movie rentals, the number of movie rentals and the number of active customers.
+
+SELECT 
+	SUM(m.renting_price), 
+	COUNT(*), 
+	COUNT(DISTINCT r.customer_id)
+FROM renting AS r
+LEFT JOIN movies AS m
+ON r.movie_id = m.movie_id
+-- Only look at movie rentals in 2018
+WHERE date_renting between '2018-01-01' and '2018-12-31' ;
+
+ SELECT m.title, -- Create a list of movie titles and actor names
+       a.name
+FROM actsin ai
+LEFT JOIN movies AS m
+ON m.movie_id = ai.movie_id
+LEFT JOIN actors AS a
+ON a.actor_id = ai.actor_id;
