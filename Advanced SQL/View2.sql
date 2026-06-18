e6e9cb5c370bdcab257d13aeffc92bf911c9a8e7
AjitaGiri <ajitagiri07@gmail.com>
2026-06-18 14:33:07 +0545

    View Security

--- Advanced SQL/View2.sql
+++ Advanced SQL/View2.sql
@@ -19,3 +19,26 @@ CREATE VIEW Sales.V_Order_Details AS (
     LEFT JOIN Sales.Employees e
     ON o.SalesPersonID=e.EmployeeID
 )
+
+-- EXCLUDE DATA FROM COUNTRY USA
+CREATE VIEW Sales.V_Order_Details_EU AS (      
+    SELECT
+    o.OrderID,
+    o.OrderDate,
+    p.Product,
+    p.Category,
+    CONCAT(COALESCE(c.FirstName,'') ,' ' ,COALESCE(c.LastName,'')) AS CustomerName,
+    c.Country,
+    CONCAT(COALESCE(e.FirstName,''),'',COALESCE(e.LastName,'')) AS SalesName,
+    e.Department,
+    o.Sales,
+    o.Quantity
+    FROM Sales.Orders o
+    LEFT JOIN Sales.Products p
+    ON o.ProductID=p.ProductID
+    LEFT JOIN Sales.Customers c
+    ON o.CustomerID=c.CustomerID
+    LEFT JOIN Sales.Employees e
+    ON o.SalesPersonID=e.EmployeeID
+    WHERE c.Country !='USA'
+)
